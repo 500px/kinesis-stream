2817a2194a9fdf2671571f49216cac7148734cf2
Umair Kayani <umair@500px.com>
2018-11-12 12:34:01 -0500

    - cleanup examples

--- examples/src/main/scala/Main.scala
+++ examples/src/main/scala/Main.scala
@@ -1,8 +1,8 @@
-import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.actor.ActorSystem
 import akka.event.{Logging, LoggingAdapter}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Keep, Sink}
-import px.kinesis.stream.consumer.Consumer
+import px.kinesis.stream.consumer.{Consumer, ConsumerConfig}
 
 object Main extends App {
 
@@ -15,14 +15,11 @@ object Main extends App {
   val consumer = Sink.foreach[String](s => logging.info(s))
   val ignore = Sink.ignore
 
-  val streamName = "inbox"
-  val appName = "test-kcl-x1"
-
   // Attach a MergeHub Source to the consumer. This will materialize to a
   // corresponding Sink.
   val runnableGraph =
     Consumer
-      .source(streamName, appName)
+      .source(ConsumerConfig.fromConfig(system.settings.config.getConfig("consumer")))
       .mapAsync(8)(r => r.markProcessed().map(_ => r))
       //.map(r => s"${r.sequenceNumber.takeRight(10)} /${r.shardId} - ${r.key}")
       .toMat(ignore)(Keep.left)
