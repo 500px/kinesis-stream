6516935f5e989b4e880f4bf3d0d0754c97509271
Nigel Benns <nigelbenns@rogers.com>
2019-08-22 13:44:47 -0400

    move openssl decrypt into release to allow external PRs to function that don't have access to the IV variables

--- release.sh
+++ release.sh
@@ -3,6 +3,9 @@ set -e
 
 tag=$1
 
+echo "Decrypting GPG secret keyring"
+openssl aes-256-cbc -K $encrypted_04c98034e207_key -iv $encrypted_04c98034e207_iv -in gpg/secring.asc.enc -out gpg/secring.asc -d
+
 echo "Setting version for project to $tag"
 echo "version in ThisBuild := \"$tag\"" > version.sbt
 
